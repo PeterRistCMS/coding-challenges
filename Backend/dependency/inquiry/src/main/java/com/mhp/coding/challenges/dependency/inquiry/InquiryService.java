ae3cf9775e6cae540175e8a27e52ffffc1ed067e
Peter Rist <petercms@infopower.net>
2019-09-10 19:44:18 +0200

    Remove second solution

--- Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
+++ Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
@@ -1,10 +1,7 @@
 package com.mhp.coding.challenges.dependency.inquiry;
 
-import com.mhp.coding.challenges.dependency.notifications.EmailHandler;
-import com.mhp.coding.challenges.dependency.notifications.PushNotificationHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
@@ -19,22 +16,11 @@ public class InquiryService{
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
-    @Autowired
-    private EmailHandler emailHandler;
-
-    @Autowired
-    private PushNotificationHandler pushNotificationHandler;
-
     public void create(final Inquiry inquiry) {
         LOG.info("User sent inquiry: {}", inquiry);
 
-        //Solution 1
         InquiryEvent inquiryEvent = new InquiryEvent(this, inquiry);
         applicationEventPublisher.publishEvent(inquiryEvent);
-
-        //Solution 2
-//        emailHandler.sendEmail(inquiry);
-//        pushNotificationHandler.sendNotification(inquiry);
     }
 
 }
