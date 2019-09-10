64b007926823e470ff991e5c66fd4dbe4be427ff
Peter Rist <petercms@infopower.net>
2019-09-10 15:24:59 +0200

    Add second solution to dependency problem

--- Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
+++ Backend/dependency/inquiry/src/main/java/com/mhp/coding/challenges/dependency/inquiry/InquiryService.java
@@ -1,7 +1,10 @@
 package com.mhp.coding.challenges.dependency.inquiry;
 
+import com.mhp.coding.challenges.dependency.notifications.EmailHandler;
+import com.mhp.coding.challenges.dependency.notifications.PushNotificationHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +19,22 @@ public class InquiryService{
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
+    @Autowired
+    private EmailHandler emailHandler;
+
+    @Autowired
+    private PushNotificationHandler pushNotificationHandler;
+
     public void create(final Inquiry inquiry) {
         LOG.info("User sent inquiry: {}", inquiry);
 
+        //Solution 1
         InquiryEvent inquiryEvent = new InquiryEvent(this, inquiry);
         applicationEventPublisher.publishEvent(inquiryEvent);
+
+        //Solution 2
+//        emailHandler.sendEmail(inquiry);
+//        pushNotificationHandler.sendNotification(inquiry);
     }
 
 }
